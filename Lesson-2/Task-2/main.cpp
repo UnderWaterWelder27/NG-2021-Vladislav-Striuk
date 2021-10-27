974e66f093995fd34d49e8df1285ce34cd9c3273
Vladislav <vvlad1175@gmail.com>
2021-10-27 10:32:06 +0300

    hash of number (for-if)

--- Lesson-2/Task-2/main.cpp
+++ Lesson-2/Task-2/main.cpp
@@ -4,19 +4,16 @@ using namespace std;
 
 int main()
 {
-    unsigned long long int num, d = 1;  //d - division coefficient
-    unsigned short int near_1, near_2, n_hash, first;
-    cout << "Enter any positive integer number: ";
-    cin >> num;
+    long long int num;
+    short int near_1, near_2, n_hash, first;
+    cout << "Enter any positive integer number: "; cin >> num;
     first = num % 10;                   //storing the minimum digit
-    do {
-        near_1 = (num/d) % 10;          //saving digit
-        near_2 = (num/(d*10)) % 10;     //saving near digit
-        d *= 10;                        //increase division coefficient
-        if (near_1 == near_2) {         //if neighbor digits are equal
+    for (long long int i = num; i >= 1; i /= 10) {
+        near_1 = i % 10;                //saving digit
+        near_2 = i/10 % 10;             //saving near digit
+        if (near_1 == near_2)           //if neighbor digits are equal
             n_hash += near_1;           //digit hash += one of the neighbor digits
-        }
-    } while (num/d >= 1);               //end of cycle if num/d < 1;
+    }
     if (near_1 == first) {              //if opposite digits are equal
         n_hash += near_1;               //digit hash += one of the opposite digits
     }
