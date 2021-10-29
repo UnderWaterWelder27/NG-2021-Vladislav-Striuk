47ded458d33159f2af44ef73365a450cde7c5c91
Vladislav <vvlad1175@gmail.com>
2021-10-29 23:52:24 +0300

    bankomat (beta 2.0) added comment, 'do' replaced 'while'

--- Lesson-3/Task-2/main.cpp
+++ Lesson-3/Task-2/main.cpp
@@ -5,160 +5,63 @@ using namespace std;
 
 int main()
 {
-    int cash[10], pincode[10], acc, save_pin, action;
-    for (int i = 0; i < 10; i++) {
-        cash[i] = rand();
-        cout << "cash [" << i << "] = " << cash[i] << "    ";
-        pincode[i] = rand() % 10000;
-        cout << "pincode [" << i << "] = " << pincode[i] << endl;
+/************************************************************************************************/
+    int cash[10], pincode[10], acc, save_value, action;
+    for (int i = 0; i < 10; i++) {                              //index loop iteration
+        cash[i] = rand();                                       //set value for cash[i]
+        pincode[i] = rand() % 10000;                            //set value for pincode[i]
+        cout << "#" << i+1 << " pincode [" << i << "] = " << pincode[i] << endl;
     }
-
-    do {                                //main cycle
-        do {                            //bank account/pincode cycle
-            cout << "Bank account: "; cin >> acc;
-            if (acc > 0 && acc <= 10) {
-                do {
-                    cout << "PIN for " << acc << " account: "; cin >> save_pin;
-                    if (save_pin != pincode[acc-1])
+/************************************************************************************************/
+    start_pos: while (true) {                                              ///main infinite cycle
+        while (!(acc > 0 && acc <= 10)) {                       ///bank account/pincode cycle
+            cout << endl << "Bank account: ";
+            cin >> acc;
+            if (acc > 0 && acc <= 10) {                         //check: does the account exist
+                do {                                            //pincode cycle
+                    cout << "PIN for " << acc << " account: ";
+                    cin >> save_value;
+                    if (save_value != pincode[acc-1])             //case: incorrect password
                         cout << "Your password is incorrect!" << endl;
-                } while (save_pin != pincode[acc-1]);
+                } while (save_value != pincode[acc-1]);
             }
             else
-                cout << "Account does not exist" << endl;
-        } while (!(acc > 0 && acc <= 10));
-
-        do {                        //cab i do cycle (BUG)
+                cout << "Account does not exist" << endl;       //if account does exist
+        }
+/************************************************************************************************/
+        while (true) {                                          ///add/withdraw cycle
             cout << "Your money: " << cash[acc-1] << " $" << endl;
             cout << "What I can do?" << endl;
             cout << "1 - add money" << endl;
             cout << "2 - withdraw money" << endl;
 
-            cin >> action;
-            if (action == 1) {
-                do {
-                    cout << "How much money to add to your account?: ";
-                    cin >> action;
-                    if (action >= 0) {
-                        cash[acc-1] += action;
-                        break;
-                    }
-                    else cout << "Incorrect value . . . " << endl;
-                } while (action < 0);
+            cin >> action;                                      //read action (1 or 2)
+            /*==================================================================================*/
+            if (action == 1) {                                  ///action 1 - add money
+                cout << "How much money to add to your account?: ";
+                cin >> save_value;                              //read action for add
+                if (save_value >= 0)
+                    cash[acc-1] += save_value;                  //account increase
+                else
+                    cout << "Incorrect value . . . " << endl;
             }
-
-            else if (action == 2) {
-                do {
-                    cout << "How much money to withdraw?: ";
-                    cin >> action;
-                    if (action <= cash[acc-1] && action >= 0) {
-                        cash[acc-1] -= action;
-                        break;
-                    }
-                    else
-                        cout << "There are not enough money in the account" << endl;
-                } while (action > cash[acc-1]);
+            /*==================================================================================*/
+            else if (action == 2) {                             ///action 2 - withdraw money
+                cout << "How much money to withdraw?: ";
+                cin >> save_value;                              //read action for divide
+                if (save_value <= cash[acc-1] && save_value >= 0)
+                    cash[acc-1] -= save_value;                  //account decrease
+                else
+                    cout << "There are not enough money in the account" << endl;
             }
-
-            else
+            /*==================================================================================*/
+            else                                                ///if action != 1 or 2
                 cout << "Unacceptable action! Try again." << endl << endl;
-        } while (action != 1 && action !=2);
-    } while (true);
-
-    /*switch (acc) {
-        case 1:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        case 2:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        case 3:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        case 4:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        case 5:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        case 6:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        case 7:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        case 8:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        case 9:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        case 10:
-        do {
-            cout << "PIN for " << acc << " account: "; cin >> save_pin;
-            acc -= 1;
-            if (save_pin != pincode[acc])
-                cout << "Your password is incorrect!";
-        } while (save_pin != pincode[acc]);
-        break;
-
-        default: cout << "Account does not exist";
-    }*/
-
-
+            break;
+        }
+        goto start_pos;
+    }
+/************************************************************************************************/
     system("pause");
     return 0;
 }
