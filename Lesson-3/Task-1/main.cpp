df5bdc952404af71999759537a231fff8f51df55
Vladislav <vvlad1175@gmail.com>
2021-10-30 11:42:23 +0300

    Increase the values of the array[10] (add comments)

--- Lesson-3/Task-1/main.cpp
+++ Lesson-3/Task-1/main.cpp
@@ -5,15 +5,16 @@ using namespace std;
 int main()
 {
     int mass[10], add;
-    for (int i = 0; i < 10; i++) {
-        cout << "mass[" << i << "] = "; cin >> mass[i];
+    for (int i = 0; i < 10; i++) {          //index iteration loop
+        cout << "mass[" << i << "] = ";
+        cin >> mass[i];                     //enter values for array
     } cout << endl;
 
     cout << "Value for increase all elements of the array: ";
     cin >> add;
 
-    for (int i = 0; i < 10; i++) {
-        mass[i] += add;
+    for (int i = 0; i < 10; i++) {          //index iteration loop
+        mass[i] += add;                     //add value for every array element
         cout << "mass[" << i << "] = " << mass[i] << endl;
     }
     return 0;
