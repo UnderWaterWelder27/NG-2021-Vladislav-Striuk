9b3b4e4c812b0c0b2504d3624b8a5b1ed89d0dfc
Vladislav <vvlad1175@gmail.com>
2021-10-31 15:13:30 +0300

    include iterated variables into for-cycles

--- Lesson-2/Task-3/main.cpp
+++ Lesson-2/Task-3/main.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 int main()
 {
-    unsigned int l, x, y;
+    int l;
     cout << "Enter length of square (integer & positive number): ";
-    cin >> l;                           //length input
-    for (y = 1; y <= l; y++) {          //for each line
-        for (x = 1; x <= l; x++) {      //for each column
-            if ((y < l && y > 1) && (x < l && x > 1)) //inner emptiness
-            cout << "  ";
-            else cout << "* ";          //outer contour
+    cin >> l;                                           //length input
+    for (int y = 1; y <= l; y++) {                      //for each line
+        for (int x = 1; x <= l; x++) {                  //for each column
+            if ((y < l && y > 1) && (x < l && x > 1))   //inner emptiness
+                cout << "  ";
+            else                                        //outer contour
+                cout << "* ";
         }
         cout << endl;
     }
