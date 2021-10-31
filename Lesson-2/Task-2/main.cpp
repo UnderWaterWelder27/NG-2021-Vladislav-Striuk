74cca207457018ef7cefb1e03dbe181516c3ef29
Vladislav <vvlad1175@gmail.com>
2021-10-31 15:02:18 +0300

    delete one variable

--- Lesson-2/Task-2/main.cpp
+++ Lesson-2/Task-2/main.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main()
 {
     long long int num;
-    short int near_1, near_2, n_hash, first;
+    short int near_1, near_2, n_hash;   //storing the minimum digit
     cout << "Enter any positive integer number: "; cin >> num;
-    first = num % 10;                   //storing the minimum digit
+
     for (long long int i = num; i >= 1; i /= 10) {
         near_1 = i % 10;                //saving digit
         near_2 = i/10 % 10;             //saving near digit
         if (near_1 == near_2)           //if neighbor digits are equal
             n_hash += near_1;           //digit hash += one of the neighbor digits
     }
-    if (near_1 == first) {              //if opposite digits are equal
+    if (near_1 == num % 10) {           //if opposite digits are equal
         n_hash += near_1;               //digit hash += one of the opposite digits
     }
     cout << "Hash of your number is: " << n_hash << endl;
