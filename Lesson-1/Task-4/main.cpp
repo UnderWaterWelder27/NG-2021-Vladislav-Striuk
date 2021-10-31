13e7ee14fc853881f4e259e159e734ca70a08c68
Vladislav <vvlad1175@gmail.com>
2021-10-31 14:48:51 +0300

    update initializing

--- Lesson-1/Task-4/main.cpp
+++ Lesson-1/Task-4/main.cpp
@@ -5,25 +5,25 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
     cout << "Enter coefficients (a, b, c) for ax^2 + bx + c = 0" << endl;
-    cout << "a = "; cin >> a;
-    cout << "b = "; cin >> b;
-    cout << "c = "; cin >> c;
+    int a; cout << "a = "; cin >> a;
+    int b; cout << "b = "; cin >> b;
+    int c; cout << "c = "; cin >> c;
 
     double D = pow(b, 2) - 4*a*c;
-    if ( D < 0) {
-        cout << endl << "D < 0" << endl;
-        cout << "No solution . . ." << endl;
+
+    if (D > 0) {
+        cout << endl << "D = " << D << endl;
+        cout << "x1 = " << (-b + sqrt(D))/2*a << endl;
+        cout << "x2 = " << (-b - sqrt(D))/2*a << endl;
     }
     else if (D == 0) {
         cout << endl << "D = 0" << endl;
         cout << "x = " << -b/2*a << endl;
     }
     else {
-        cout << endl << "D = " << D << endl;
-        cout << "x1 = " << (-b + sqrt(D))/2*a << endl;
-        cout << "x2 = " << (-b - sqrt(D))/2*a << endl;
+        cout << endl << "D < 0" << endl;
+        cout << "No solution . . ." << endl;
     }
     return 0;
 }
