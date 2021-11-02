6c93a466c7cbcb524b358ffc46c8e037bae021ec
Vladislav <vvlad1175@gmail.com>
2021-11-02 13:57:32 +0200

    variables are initialized

--- Lesson-2/Task-1/main.cpp
+++ Lesson-2/Task-1/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int num, sum;
+    int num = 0, sum = 0;
     cout << "Enter any positive integer number: "; cin >> num;
     for (int i = num; i >= 1; i /= 10) {
         sum += (i % 10);
