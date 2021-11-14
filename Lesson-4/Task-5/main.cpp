ac0298e25fc77033839e76ca453afd6f57fb9d25
Vladislav <vvlad1175@gmail.com>
2021-11-14 13:59:07 +0200

    anti-badlanguage filter

---
+++ Lesson-4/Task-5/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char bad_language [256] = "";
+    cin.getline(bad_language, 256);
+    for (int i = 0; i < strlen(bad_language); i++) {
+        if (bad_language[i] == 'f' || bad_language[i+1] == 'u' ||
+            bad_language[i+2] == 'c' || bad_language[i+3] == 'k' ) {
+                bad_language[i]   = '*';
+                bad_language[i+1] = '*';
+                bad_language[i+2] = '*';
+                bad_language[i+3] = '*';
+        }
+        cout << bad_language[i];
+    }
+    return 0;
+}
