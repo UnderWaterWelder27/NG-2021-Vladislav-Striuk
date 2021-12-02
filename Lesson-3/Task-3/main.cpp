5b774949ad11d809155e108a32f1a7f8aa1439f1
Vladislav <vvlad1175@gmail.com>
2021-12-02 10:16:16 +0200

    removed multiplicative (10*, 100*...). added math pow. optimized to one for-cycle

--- Lesson-3/Task-3/main.cpp
+++ Lesson-3/Task-3/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <time.h>
+#include <math.h>
 
 using namespace std;
 
@@ -8,22 +9,21 @@ int main()
 {
     srand(time(0));
 
-    int first_number = 0, second_number = 0, first_array[4] = {0}, second_array[4] = {0};
+    int first_number = 0,       second_number = 0,
+        first_array[4] = {0},   second_array[4] = {0};
 
-    for (int i = 0, digit = 1000; i < 4; i ++, digit /= 10) {
+///     i - for every array elements from first to last
+///     j - for pow of digit for multiplying (10, 100, 1000)
+
+    for (int i = 0, j = 3; j >= 0; i ++, j--) {
         first_array[i] = rand()%10;                         //assignment first_array[]
-        cout << first_array[i] << " ";
-        first_array[i] *= digit;                            //first_array[i] * digit in number
-        first_number += first_array[i];                     //sum of the digits from first_array (complete number)
-    }
-    cout << endl;
-    for (int i = 0, digit = 1000; i < 4; i ++, digit /= 10) {
+        first_number += first_array[i] * pow(10, j);        //sum of the digits from first_array (complete number)
         second_array[i] = rand()%10;                        //assignment second_array[]
-        cout << second_array[i] << " ";
-        second_array[i] *= digit;                           //second_array[i] * digit in number
-        second_number += second_array[i];                   //sum of the digits from second_array(complete number)
+        second_number += second_array[i] * pow(10, j);      //sum of the digits from second_array(complete number)
     }
-    cout << endl <<  first_number << " + " << second_number
-    << " = " << first_number + second_number << endl;
+    cout <<  first_number << " + " << second_number
+         << " = " << first_number + second_number
+         << endl;
+
     return 0;
 }
