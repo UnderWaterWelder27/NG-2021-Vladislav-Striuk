82702e90e65da1d73f6ac2710e947457a52132ca
Vladislav <vvlad1175@gmail.com>
2021-12-12 16:00:00 +0200

    return is replaced to bool

--- Lesson-4/Task-2/main.cpp
+++ Lesson-4/Task-2/main.cpp
@@ -4,15 +4,18 @@ using namespace std;
 
 int main()
 {
+    bool equivalent = true;
     char first_string[255] = "", second_string[255] = "";
     cin.getline(first_string, 255);
     cin.getline(second_string, 255);
     for (int i = 0; i < 255; i++) {
-        if (first_string[i] != second_string[i]) {
-            cout << "Strings are not equivalent!" << endl;
-            return 0;
-        }
+        if (first_string[i] != second_string[i])
+            equivalent = false;
     }
-    cout << "These strings are the same" << endl;
+
+    if (equivalent)
+        cout << "These strings are the same" << endl;
+    else
+        cout << "Strings are not equivalent!" << endl;
     return 0;
 }
