05a4d2b0f574952517dee894b6abe4755d61cb15
Vladislav <vvlad1175@gmail.com>
2021-12-18 12:32:31 +0200

    simplified for-cycle (conditions are merged)

--- Lesson-2/Task-2/main.cpp
+++ Lesson-2/Task-2/main.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main()
 {
     long long num = 0;
-    short int digit = 0, n_hash = 0;
-    cout << "Enter any positive integer number: "; cin >> num;
+
+    cout << "Enter any positive integer number: ";
+    cin >> num;
+
+    short int digit  = num % 10,                    //saving digit num % 10
+              n_hash = 0;
 
     for (long long i = num; i >= 1; i /= 10) {
-        digit = i % 10;                //saving digit
-        if (digit == i/10 % 10)        //if neighbor digits are equal
-            n_hash += digit;           //digit hash += one of the neighbor digits
-    }
-    if (digit == num % 10) {           //if opposite digits are equal
-        n_hash += digit;               //digit hash += one of the opposite digits
+        if (i % 10 == i/10 % 10 || digit == i)      //if neighbor and opposite digits are equal
+            n_hash += i % 10;                       //digit hash += one of the neighbor/opposite digits
     }
     cout << "Hash of your number is: " << n_hash << endl;
     return 0;
