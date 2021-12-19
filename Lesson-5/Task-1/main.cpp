4a5689790394ec4cd18478da3fc9cdc89f3b00cb
Vladislav <vvlad1175@gmail.com>
2021-12-19 13:24:21 +0200

    all cycles were replaced to recursive functions

--- Lesson-5/Task-1/main.cpp
+++ Lesson-5/Task-1/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int input_lenght ()                                                                 //length input
+int input_lenght ()                                         // function length input
 {
     int lenght = 0;
     cout << "Enter length of square (integer & positive number): ";
@@ -10,22 +10,30 @@ int input_lenght ()                                                             
     return lenght;
 }
 
-void output_square (int lenght)
+void line_output (int lenght, int y, int x)                 // function of line output
 {
-    for (int y = 1; y <= lenght; y++)                                               //for each line
+    if (x <= lenght)                                        // repeat condition for every column
     {
-        for (int x = 1; x <= lenght; x++)                                           //for each column
-        {
-            if ((y < lenght && y > 1) && (x < lenght && x > 1))     cout << "  ";   //inner emptiness
-            else    cout << "* ";                                                   //outer contour
-        }
-        cout << endl;
+        if ((y < lenght && y > 1) && (x < lenght && x > 1)) // for emptiness
+            cout << "  ";
+        else                                                // for frame
+            cout << "* ";
+        line_output(lenght, y, x += 1);                     // recursion for the next elementin line
+    }
+}
+
+void square_output (int lenght, int y, int x)               // function for output every line in square
+{
+    line_output(lenght, y, x);                              // line output
+    cout << endl;
+    if (y < lenght) {                                       // repeat condition for every line
+        square_output(lenght, y += 1, x);                   // recursion for the next line
     }
 }
 
 int main()
 {
-    int square_lenght = input_lenght();
-    output_square(square_lenght);
+    int square_lenght = input_lenght();                     // input lenght
+    square_output(square_lenght, 1, 1);                     // output square frame
     return 0;
 }
