194b0620000f3e7c2be2a247a85d05cc1e08e1f9
Vladislav <vvlad1175@gmail.com>
2021-12-20 12:47:03 +0200

    optimized function realizations (removed excess arguments)

--- Lesson-5/Task-2/main.cpp
+++ Lesson-5/Task-2/main.cpp
@@ -3,108 +3,100 @@
 
 using namespace std;
 
-int main();
-int bank_cash[10] = {0}, pincod[10] = {0}, acc = 0, value = 0;
-bool setter = false;
-
-void Set_Cash_and_Pincod(int m_bank_cash[10], int m_pincod[10])
+void SetCashAndPincod(int bank_cash[], int pincod[])                        //  initialize cash & pincod
 {
-    for (int i = 0; i < 10; i++) {                                          //index loop iteration
-        m_bank_cash[i] = rand();                                            //set value for bank cash
-        m_pincod[i] = rand() % 10000;                                       //set value for pincod
+    for (int i = 0; i < 10; i++) {
+        bank_cash[i] = rand();                                              /// set value for bank cash
+        pincod[i] = rand() % 10000;                                         /// set value for pincod
     }
 }
 
-void Get_Pincod(int m_pincod[10])
+void GetPincod(int pincod[])                                                // pincod output
 {
-    for (int i = 0; i < 10; i++) {                                          //pincod list
-        cout << "#" << i+1 << " pincode = " << m_pincod[i] << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "#" << i+1 << " pincode = " << pincod[i] << endl;
     }
 }
 
-int Bank_Account(int m_acc)
+int AskForBankAccount()                                                     //  input bank account №
 {
-    cout << endl << endl << "Bank account: ";
-    cin >> m_acc;
-    if (!(m_acc > 0 && m_acc <= 10))                                        //check: does the account exist
-        main();
-    return m_acc;
+    int acc = 0;
+    do {
+        cout << endl << endl << "Bank account: ";
+        cin >> acc;
+    } while (acc <= 0 || acc > 10);                                         /// check: does the account exist
+    return acc;
 }
 
-int Pincod_Account(int m_pincod[10], int m_acc, int m_value)
+void AskForPincod(int pincod, int acc)                                      //  input pincod for n-account
 {
-    cout << "PIN for " << m_acc << " account: ";
-    cin >> m_value;
-    if (m_value != m_pincod[m_acc-1])                                       //case: incorrect password
-        main();
-    return m_pincod[m_acc-1];
+    int value = 0;
+    do {
+        cout << "PIN for " << acc << " account: ";
+        cin >> value;
+    } while (value != pincod);                                              /// check: does the password is correct
 }
 
-int Action_Question(int m_bank_cash[10], int m_acc, int m_value)
+int ChooseAction(int bank_cash)                                             //  choosing the -add- or -withdraw- action
 {
-    cout << "Money on your card: "  << m_bank_cash[m_acc-1] << " $" << endl;
-    cout << "What I can do?"        << endl;
-    cout << "1 - add money"         << endl;
-    cout << "2 - withdraw money"    << endl;
-    cin >> m_value;                                                         //read action (1 or 2)
-
-    if (m_value != 1 && m_value != 2)
-        main();
-    return m_value;
+    int value = 0;
+    do {
+        cout << "Money on your card: "  << bank_cash << " $" << endl;
+        cout << "What I can do?"        << endl;
+        cout << "1 - add money"         << endl;
+        cout << "2 - withdraw money"    << endl;
+        cin >> value;                                                       /// check: does the action equals 1 or 2
+    } while (value < 1 || value > 2);
+    return value;
 }
 
-void Add_Money(int m_bank_cash[10], int m_acc, int m_value)
+int AddMoney()                                                              //  add money to bank cash
 {
-    cout << "How much money to add to your account?: ";
-    cin >> m_value;                                                         //read action for add
-    if (m_value >= 0)
-    {                                                                       //check: is there enough hard cash
-        m_bank_cash[m_acc-1] += m_value;                                    //bank cash increase
-        cout << "Transaction successful!";
-        main();
-    }
-    else
-    {
-        cout << "Unacceptable count! Try again." << endl << endl;
-        main();
-    }
+    int value = 0;
+    do {
+        cout << "How much money to add to your account?: ";
+        cin >> value;
+    } while (value < 0);                                                    /// check: does the value no less then 0
+    return value;
 }
 
-void Divide_Money(int m_bank_cash[10], int m_acc, int m_value)
+int WithdrawMoney(int bank_cash)                                            //  withdraw money from bank cash
 {
-    cout << "How much money to withdraw?: ";
-    cin >> m_value;                                                         //read action for divide
-    if (m_value >= 0 && m_value <= m_bank_cash[m_acc-1])
-    {                                                                       //check: is there enough bank cash
-        m_bank_cash[m_acc-1] -= m_value;                                    //bank cash decrease
-        cout << "Transaction successful!";
-        main();
-    }
-    else
-    {
-        cout << "Unacceptable count! Try again." << endl << endl;
-        main();
-    }
+    int value = 0;
+    do {
+        cout << "How much money to withdraw?: ";
+        cin >> value;
+    } while (bank_cash - value < 0);                                        /// check: is there enough bank cash
+    return value;
 }
 
 int main()
 {
-    if (setter == false)
+    int bank_cash[10] = {0}, pincod[10] = {0}, acc = 0, value = 0;
+    SetCashAndPincod(bank_cash, pincod);
+    while (true)
     {
-        Set_Cash_and_Pincod(bank_cash, pincod);
-        setter = true;
-    }
-    system ("cls");                                                         //console clear
-    Get_Pincod(pincod);
+        system ("cls");                                                     //  console clear
+        GetPincod(pincod);
 
-    acc = Bank_Account(acc);
-    pincod[acc-1] = Pincod_Account(pincod, acc, value);
-    value = Action_Question(bank_cash, acc, value);
+        acc = AskForBankAccount();
+        AskForPincod(pincod[acc-1], acc);
+        value = ChooseAction(bank_cash[acc-1]);
 
-    switch (value)
-    {
-        case 1: Add_Money(bank_cash, acc, value);                           ///action 1 - add money
-        case 2: Divide_Money(bank_cash, acc, value);                        ///action 2 - withdraw money
+        switch (value)
+        {
+        case 1:                                                             //  action 1 - add money
+            value = AddMoney();
+            bank_cash[acc-1] += value;                                      /// bank cash increase
+            cout << "Transaction successful!";
+            break;
+
+        case 2:                                                             //  action 2 - withdraw money
+            value = WithdrawMoney(bank_cash[acc-1]);
+            bank_cash[acc-1] -= value;                                      /// bank cash decrease
+            cout << "Transaction successful!";
+            break;
+        }
     }
     return 0;
 }
