96481ea225aabee6d1d1473c9ae38281d2853ef9
Vladislav <vvlad1175@gmail.com>
2021-12-20 19:03:51 +0200

    simlified cycle condition; added uppercase symbols

--- Lesson-4/Task-4/main.cpp
+++ Lesson-4/Task-4/main.cpp
@@ -7,19 +7,17 @@ int main()
     char sentence[256] = "";
     int word_hash = 0;
     cin.getline(sentence, 256);
-    for (int i = 0; i < 256; i++) {
-        if (sentence[i] == 'a' || sentence[i] == 'e' ||
-            sentence[i] == 'i' || sentence[i] == 'o' ||
-            sentence[i] == 'u' || sentence[i] == 'y'   )
+
+    for (int i = 0; sentence[i] != '\0'; i++) {
+        if (sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] == 'A' || sentence[i] == 'E' ||
+            sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'I' || sentence[i] == 'O' ||
+            sentence[i] == 'u' || sentence[i] == 'y' || sentence[i] == 'U' || sentence[i] == 'Y'   )
             word_hash += 1;
-        if (sentence[i] == ' ') {
+
+        if (sentence[i] == ' ' || sentence[i+1] == '\0') {
             cout << word_hash;
             word_hash = 0;
             continue;
-            }
-        if (sentence[i] == '\0') {
-            cout << word_hash;
-            break;
         }
     }
     return 0;
