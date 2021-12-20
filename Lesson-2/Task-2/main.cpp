c127bd622ee24f0c088543f8a219be0d4ff9c786
Vladislav <vvlad1175@gmail.com>
2021-12-20 13:28:44 +0200

    for-cycle replaced to while-cycle

--- Lesson-2/Task-2/main.cpp
+++ Lesson-2/Task-2/main.cpp
@@ -9,13 +9,17 @@ int main()
     cout << "Enter any positive integer number: ";
     cin >> num;
 
-    short int digit  = num % 10,                    //saving digit num % 10
+    short int digit  = num % 10,                        //saving digit num % 10
               n_hash = 0;
 
-    for (long long i = num; i >= 1; i /= 10) {
-        if (i % 10 == i/10 % 10 || digit == i)      //if neighbor and opposite digits are equal
-            n_hash += i % 10;                       //digit hash += one of the neighbor/opposite digits
+    long long i = num;
+    while (i >= 1)
+    {
+        if (i % 10 == i/10 % 10 || digit == i)          //if neighbor and opposite digits are equal
+            n_hash += i % 10;                           //digit hash += one of the neighbor/opposite digits
+        i /= 10;
     }
+
     cout << "Hash of your number is: " << n_hash << endl;
     return 0;
 }
