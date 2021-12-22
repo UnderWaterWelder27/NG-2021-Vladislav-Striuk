b440e7c8fc5410b83ab4a05a49c7e6aafa4ce6be
Vladislav <vvlad1175@gmail.com>
2021-12-22 14:16:34 +0200

    simplified function interface

--- Lesson-5/Task-1/main.cpp
+++ Lesson-5/Task-1/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int input_lenght ()                                         // function length input
+int LenghtInput ()                                          // function length input
 {
     int lenght = 0;
     cout << "Enter length of square (integer & positive number): ";
@@ -10,30 +10,33 @@ int input_lenght ()                                         // function length i
     return lenght;
 }
 
-void line_output (int lenght, int y, int x)                 // function of line output
+void FrameBuilding (int lenght, int y, int x)               // function of line output
 {
-    if (x <= lenght)                                        // repeat condition for every column
+    if (x <= lenght && y <= lenght)                         // repeat condition for every column
     {
         if ((y < lenght && y > 1) && (x < lenght && x > 1)) // for emptiness
             cout << "  ";
         else                                                // for frame
             cout << "* ";
-        line_output(lenght, y, x += 1);                     // recursion for the next elementin line
+        FrameBuilding(lenght, y, x + 1);                    // recursion for the next elementin line
+
     }
+    if (x == lenght + 1)                                    // repeat condition for every line
+    {
+        cout << endl;
+        FrameBuilding(lenght, y + 1, 1);                    // recursion for the next line
+    }
+
 }
 
-void square_output (int lenght, int y, int x)               // function for output every line in square
+void DrawSquare (int lenght)                                // function for output every line in square
 {
-    line_output(lenght, y, x);                              // line output
-    cout << endl;
-    if (y < lenght) {                                       // repeat condition for every line
-        square_output(lenght, y += 1, x);                   // recursion for the next line
-    }
+    FrameBuilding(lenght, 1, 1);                            // line output
 }
 
 int main()
 {
-    int square_lenght = input_lenght();                     // input lenght
-    square_output(square_lenght, 1, 1);                     // output square frame
+    int lenght = LenghtInput();                             // input lenght
+    DrawSquare(lenght);                                     // output square frame
     return 0;
 }
