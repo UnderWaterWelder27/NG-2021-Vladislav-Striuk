1ed32042ad72c82c75528ce56a6a1cdb445b7176
Vladislav <vvlad1175@gmail.com>
2022-01-16 15:04:43 +0200

    fixed world frame bug / default map '.' changed to ' '

--- Lesson-3/Task-4-incredible/main.cpp
+++ Lesson-3/Task-4-incredible/main.cpp
@@ -6,76 +6,90 @@ using namespace std;
 
 int main()
 {
-/**************************************************************************************************************************************/
-///INITIALIZING VARIABLES
-
+    ///INITIALIZING VARIABLES
     srand(time(0));
-    const int world_size_x = 100;                                               //width of the world
-    const int world_size_y = 40;                                                //height of the world
+    const int WORLD_SIZE_X = 100;                                           //width of the world
+    const int WORLD_SIZE_Y = 20;                                            //height of the world
 
-    char general_map [world_size_y][world_size_x];                              //main map[][]
-    char player_map [world_size_y][world_size_x];                               //player map[][]
+    char generalMap [WORLD_SIZE_Y][WORLD_SIZE_X];                           //main map[][]
+    char playerMap [WORLD_SIZE_Y][WORLD_SIZE_X];                            //player map[][]
 
-    int player_pos_x = world_size_x/2;                                          //player x position
-    int player_pos_y = world_size_y/2;                                          //player y position
+    int playerPosX = WORLD_SIZE_X/2;                                        //player x position
+    int playerPosY = WORLD_SIZE_Y/2;                                        //player y position
 
-    char player_action = ' ';                                                   //player action
+    char playerAction = ' ';                                                //player action
 
-/**************************************************************************************************************************************/
-///GENERATING MAP
-
-    for (int y = 0; y < world_size_y; y++) {                                    //for Y (line)
-        for (int x = 0; x < world_size_x; x++) {                                //for X (column)
-            if (y == world_size_y - 1 || y == 0 ||
-                x == world_size_x - 1 || x == 0) {                              //coordinates of the frame
-                    general_map[y][x] = '/';                                    //inaccessible frame
+    ///GENERATING MAP
+    for (int y = 0; y < WORLD_SIZE_Y; y++) {                                //for Y (line)
+        for (int x = 0; x < WORLD_SIZE_X; x++) {                            //for X (column)
+            if (   y == WORLD_SIZE_Y - 1 || y == WORLD_SIZE_Y - 2
+                || x == WORLD_SIZE_X - 1 || x == WORLD_SIZE_X - 2
+                || y == 0 || y == 1 || x == 0 || x == 1)
+            {                                                               //coordinates of the frame
+                generalMap[y][x] = '/';                                     //inaccessible frame
             }
             else {
-                switch(rand()%23) {                                             //generating map
-                    case 1:  general_map[y][x] = 't'; break;                    //trees
-                    case 2:  general_map[y][x] = 's'; break;                    //stones
-                    default: general_map[y][x] = '.';                           //empty field
+                switch(rand()%23) {                                         //generating map
+                case 1:  generalMap[y][x] = 't'; break;                     //trees
+                case 2:  generalMap[y][x] = 's'; break;                     //stones
+                default: generalMap[y][x] = ' ';                            //empty field
                 }
             }
-            player_map[y][x] = '*';                                             //map fog
+            playerMap[y][x] = '*';                                          //map fog
         }
     }
 
-/**************************************************************************************************************************************/
-///DISCOVERING WORLD
-
+    ///DISCOVERING WORLD
     while (true) {
-        player_map[player_pos_y][player_pos_x + 1]      =    general_map[player_pos_y][player_pos_x + 1];       //right
-        player_map[player_pos_y][player_pos_x - 1]      =    general_map[player_pos_y][player_pos_x - 1];       //left
-        player_map[player_pos_y + 1][player_pos_x]      =    general_map[player_pos_y + 1][player_pos_x];       //down
-        player_map[player_pos_y - 1][player_pos_x]      =    general_map[player_pos_y - 1][player_pos_x];       //up
+        playerMap[playerPosY][playerPosX + 1]      =    generalMap[playerPosY][playerPosX + 1];         //right
+        playerMap[playerPosY][playerPosX - 1]      =    generalMap[playerPosY][playerPosX - 1];         //left
+        playerMap[playerPosY + 1][playerPosX]      =    generalMap[playerPosY + 1][playerPosX];         //down
+        playerMap[playerPosY - 1][playerPosX]      =    generalMap[playerPosY - 1][playerPosX];         //up
 
-        player_map[player_pos_y + 1][player_pos_x + 1]  =    general_map[player_pos_y + 1][player_pos_x + 1];   //down right
-        player_map[player_pos_y - 1][player_pos_x - 1]  =    general_map[player_pos_y - 1][player_pos_x - 1];   //up left
-        player_map[player_pos_y + 1][player_pos_x - 1]  =    general_map[player_pos_y + 1][player_pos_x - 1];   //down left
-        player_map[player_pos_y - 1][player_pos_x + 1]  =    general_map[player_pos_y - 1][player_pos_x + 1];   //up right
+        playerMap[playerPosY][playerPosX + 2]      =    generalMap[playerPosY][playerPosX + 2];         //double right
+        playerMap[playerPosY][playerPosX - 2]      =    generalMap[playerPosY][playerPosX - 2];         //double left
+        playerMap[playerPosY + 2][playerPosX]      =    generalMap[playerPosY + 2][playerPosX];         //double down
+        playerMap[playerPosY - 2][playerPosX]      =    generalMap[playerPosY - 2][playerPosX];         //double up
+
+        playerMap[playerPosY + 1][playerPosX + 1]  =    generalMap[playerPosY + 1][playerPosX + 1];     //down right
+        playerMap[playerPosY - 1][playerPosX - 1]  =    generalMap[playerPosY - 1][playerPosX - 1];     //up left
+        playerMap[playerPosY + 1][playerPosX - 1]  =    generalMap[playerPosY + 1][playerPosX - 1];     //down left
+        playerMap[playerPosY - 1][playerPosX + 1]  =    generalMap[playerPosY - 1][playerPosX + 1];     //up right
+
+        playerMap[playerPosY + 2][playerPosX + 1]      =    generalMap[playerPosY + 2][playerPosX + 1];
+        playerMap[playerPosY + 2][playerPosX - 1]      =    generalMap[playerPosY + 2][playerPosX - 1];
+        playerMap[playerPosY - 2][playerPosX + 1]      =    generalMap[playerPosY - 2][playerPosX + 1];
+        playerMap[playerPosY - 2][playerPosX - 1]      =    generalMap[playerPosY - 2][playerPosX - 1];
+
+        playerMap[playerPosY + 1][playerPosX + 2]      =    generalMap[playerPosY + 1][playerPosX + 2];
+        playerMap[playerPosY + 1][playerPosX - 2]      =    generalMap[playerPosY + 1][playerPosX - 2];
+        playerMap[playerPosY - 1][playerPosX + 2]      =    generalMap[playerPosY - 1][playerPosX + 2];
+        playerMap[playerPosY - 1][playerPosX - 2]      =    generalMap[playerPosY - 1][playerPosX - 2];
+
+        playerMap[playerPosY + 2][playerPosX + 2]      =    generalMap[playerPosY + 2][playerPosX + 2];
+        playerMap[playerPosY + 2][playerPosX - 2]      =    generalMap[playerPosY + 2][playerPosX - 2];
+        playerMap[playerPosY - 2][playerPosX + 2]      =    generalMap[playerPosY - 2][playerPosX + 2];
+        playerMap[playerPosY - 2][playerPosX - 2]      =    generalMap[playerPosY - 2][playerPosX - 2];
         system("cls");
 
-        for (int y = 0; y < world_size_y; y++) {
-            for (int x = 0; x < world_size_x; x++) {
-                if (player_pos_x == x && player_pos_y == y)
+        for (int y = 0; y < WORLD_SIZE_Y; y++) {
+            for (int x = 0; x < WORLD_SIZE_X; x++) {
+                if (playerPosX == x && playerPosY == y)
                     cout << "@";
                 else
-                    cout << player_map[y][x];
+                    cout << playerMap[y][x];
             } cout << endl;
         }
 
+
         cout << ">";
-        cin >> player_action;
-        switch(player_action)
-        {
-            case 'w': if (general_map[player_pos_y-1][player_pos_x] == '.') player_pos_y--; break; //movement to up
-            case 's': if (general_map[player_pos_y+1][player_pos_x] == '.') player_pos_y++; break; //movement to down
-            case 'd': if (general_map[player_pos_y][player_pos_x+1] == '.') player_pos_x++; break; //movement to right
-            case 'a': if (general_map[player_pos_y][player_pos_x-1] == '.') player_pos_x--; break; //movement to left
+        cin >> playerAction;
+        switch(playerAction) {
+        case 'w': if (generalMap[playerPosY-1][playerPosX] == ' ') playerPosY--; break;             //movement to up
+        case 's': if (generalMap[playerPosY+1][playerPosX] == ' ') playerPosY++; break;             //movement to down
+        case 'd': if (generalMap[playerPosY][playerPosX+1] == ' ') playerPosX++; break;             //movement to right
+        case 'a': if (generalMap[playerPosY][playerPosX-1] == ' ') playerPosX--; break;             //movement to left
         }
     }
-
-/**************************************************************************************************************************************/
     return 0;
 }
